90adec42962094013081397136d7846520729f9f
SuzukiTaylorSwift <150342821+SuzukiTaylorSwift@users.noreply.github.com>
2026-02-17 20:16:05 +0700

    update dataset

--- train.py
+++ train.py
@@ -1,19 +1,40 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 import pickle
 
-# โหลด dataset
 data = pd.read_csv("dataset.csv", header=None)
 
-X = data.iloc[:, :-1]   # landmark (42 ค่า)
-y = data.iloc[:, -1]    # label (A,B,C,D)
+# clean
+data = data.dropna()
 
-# สร้าง model
-model = RandomForestClassifier(n_estimators=200)
-model.fit(X, y)
+# เอาเฉพาะแถวที่ column ครบ 43
+data = data[data.apply(lambda x: len(x) == 43, axis=1)]
 
-# save model
-with open("model.pkl", "wb") as f:
-    pickle.dump(model, f)
+
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1] #label
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier(
+    n_estimators=300,
+    max_depth=20,
+    min_samples_leaf=5,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+with open("model.pkl","wb") as f:
+    pickle.dump(model,f)
 
 print("model.pkl ถูกสร้างแล้ว")
